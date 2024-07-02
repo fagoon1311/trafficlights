c202340b81ebc60ff79673b492d616105750c3c0
Fagoon Sharma <fs14smailbox65@gmail.com>
2024-07-02 16:57:36 +0000

    traffic signal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState, useEffect} from 'react'
 function App() {
+  const config = {
+    red:{
+      color:"red",
+      duration:4000,
+      next:"yellow"
+    },
+    yellow:{
+      color:"yellow",
+      duration:4000,
+      next:"green"
+    },
+    green:{
+      color:"green",
+      duration:4000,
+      next:"red"
+    }
+  }
+
+  const [color, setColor] = useState('red')
+  useEffect(()=>{
+    const { duration, next } = config[color]
+    const timerid = setTimeout(()=>{
+      setColor(next)
+      console.log(color)
+    }, duration)
+
+    return ()=>{
+      clearTimeout(timerid)
+    };
+  },[color])
+  
+  const colors = ['red', 'yellow', 'green'];
+
+  const colorClasses = {
+    red: color === 'red' ? 'bg-red-600' : 'bg-slate-100',
+    yellow: color === 'yellow' ? 'bg-yellow-400' : 'bg-slate-100',
+    green: color === 'green' ? 'bg-green-600' : 'bg-slate-100',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex items-center justify-center h-screen">
+      <div className="w-[10rem] border rounded-md h-[25rem] bg-black grid grid-rows-3 p-2">
+        {colors.map((clr) => (
+          <div key={clr} className="flex items-center justify-center">
+            <div className={`${colorClasses[clr]} rounded-full h-24 w-24`}></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
